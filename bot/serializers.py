460661c94abf54fa49c086102e1ebba706b235e7
Sanatbek Bokijonov <sanatjon.dark@gmail.com>
2024-07-30 14:50:28 +0500

    Delete bot directory

--- bot/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Dacha
-
-class DachaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Dacha
-        fields = '__all__'
-
